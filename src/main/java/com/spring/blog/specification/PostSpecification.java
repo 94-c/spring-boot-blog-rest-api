14a042acead7425e6d0053e0f1ba9e67a836a005
94-c <hyeongwoo26@gmail.com>
2023-06-30 00:50:10 +0900

    # 파일 등록 완료 -> 파일 다운로드 구현 중

---
+++ src/main/java/com/spring/blog/specification/PostSpecification.java
@@ -0,0 +1,30 @@
+package com.spring.blog.specification;
+
+import com.spring.blog.entity.Post;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class PostSpecification {
+
+    public static Specification<Post> searchTitle(String title) {
+        return new Specification<Post>() {
+            @Override
+            public Predicate toPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.like(root.get("title"), "%" + title + "%");
+            }
+        };
+    }
+
+    public static Specification<Post> searchContent(String content) {
+        return new Specification<Post>() {
+            @Override
+            public Predicate toPredicate(Root<Post> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return criteriaBuilder.like(root.get("content"), "%" + content + "%");
+            }
+        };
+    }
+}
